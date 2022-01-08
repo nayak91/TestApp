2161149a0c658eb0f630ca730b8b4899e623d5e7
nayak91 <92243226+nayak91@users.noreply.github.com>
2022-01-08 13:51:34 +0530

    Add files via upload

---
+++ src/main/java/com/example/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/*
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+
+@Repository
+*/
+public interface UserRepository //extends JpaRepository<User,Integer> , JpaSpecificationExecutor<User>
+{
+ 
+	
+} 
+ 
