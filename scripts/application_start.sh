10c7d9d388ad46dc90eef207f6077499552792cd
nayak91 <prashant072091@gmail.com>
2022-01-12 17:40:16 +0000

    dsds

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "starting server started"
-cd ../
+echo result - $(pwd)
 sudo mvn clean install
 sudo java -jar target/SpringBoot-0.0.1-SNAPSHOT.jar > test.log
 echo "server started"
