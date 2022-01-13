1607783c72a288d7ee87c5dd699b8db9d7e1b0d6
nayak91 <92243226+nayak91@users.noreply.github.com>
2022-01-13 10:02:27 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "starting server started"
 echo result - $(pwd)
+cd /home/ec2-user/codebase
+echo result2 - $(pwd)
 sudo mvn clean install
-sudo java -jar target/SpringBoot-0.0.1-SNAPSHOT.jar > test.log
+sudo java -jar /home/ec2-user/codebase/target/SpringBoot-0.0.1-SNAPSHOT.jar > test.log
 echo "server started"
